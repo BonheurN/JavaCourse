69db8975fef30b6afb1b279288572fbc331cc24a
BonheurN <84632260+BonheurN@users.noreply.github.com>
2024-05-24 13:06:41 +0200

    Update Ethod.java

--- Ethod.java
+++ Ethod.java
@@ -8,10 +8,24 @@ public class Ethod {
 		String name = "Bruno";
 		int age = 19;
 		hello(name, age);
+		methoid meth = new methoid("sulphuric acid", "acid", 721);
 	}
 
 	static void hello(String name,int age) {
 		System.out.println("Hello "+name);
 		System.out.println("You are "+age+" Years old");
+		System.out.println(meth.name);
+		System.out.println(meth.classification);
+
+	}
+}
+class methoid{
+	String name;
+	String classification;
+	int year;
+	methoid(String name, String classification, int year){
+		this.name = name;
+		this.classification = classification;
+		this.year = year;
 	}
 }
