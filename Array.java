b3c6ab23123a35c4b2fe55f6b25566b308b21e29
BonheurN <84632260+BonheurN@users.noreply.github.com>
2024-05-24 14:03:05 +0200

    Update Array.java

--- Array.java
+++ Array.java
@@ -33,6 +33,36 @@ ArrayList<String> mycollection = new ArrayList<>();
 		mycollection[2].add("Corvette");
 		mycollection[3].add("Supra");
 		System.out.println(mycollection.get(2));
+		for(String i:mycollection){
+			System.out.println(i);
+		}
+String [][]cars = new String[4][5];
+	cars[0][0] = "RAV4";
+	cars[0][1] = "Carina";
+	cars[0][2] = "hybrd";
+	cars[0][3] = "V8";
+	cars[0][4] = "motorolla";
+	cars[1][0] = "corvette";
+	cars[1][1] = "tesla";
+	cars[1][2] = "lambo";
+	cars[1][3] = "ferrari";
+	cars[1][4] = "corolla";
+	cars[2][0] = "honda";
+	cars[2][1] = "scoda";
+	cars[2][2] = "volvo";
+	cars[2][3] = "golf";
+	cars[2][4] = "hyundai";
+	cars[3][0] = "porsche";
+	cars[3][1] = "ranger";
+	cars[3][2] = "rover";
+	cars[3][3] = "merdedez";
+	cars[3][4] = "cross country";
+System.out.printl(cars[2][1]);
+		for(int i=0; i<cars.length; i++) {
+		System.out.println();
+		for(int j=0; j<cars[i].length; j++) {
+			System.out.println(cars[i][j]+ " ");
+		
 	}	
 }
 class Food{
